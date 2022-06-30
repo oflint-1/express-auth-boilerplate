ef3b9c063c46a58eecdb095180d416414badffff
oliver <oliverlenton@me.com>
2022-06-30 20:46:25 +0100

    Fix logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,12 +4,10 @@ const bcrypt = require("bcryptjs");
 
 // Login function
 async function login(req, res) {
-  console.log("test");
   res.redirect("/api/auth/status");
 }
 
 async function signup(req, res, next) {
-  console.log(req.body);
   // generate salt
   const salt = bcrypt.genSaltSync();
   // generate hash using password and salt
@@ -23,7 +21,6 @@ async function signup(req, res, next) {
 
   // save user to database
   const savedUser = await newUser.save().catch((err) => console.log(err));
-  console.log(savedUser);
 
   //If user saved correctly, login user
   if (savedUser) {
@@ -44,17 +41,20 @@ async function signup(req, res, next) {
 }
 
 async function status(req, res) {
-  console.log(req.isAuthenticated());
   if (req.isAuthenticated()) {
-    res.send(true);
+    res.send("logged in");
   } else {
-    res.send(false);
+    res.send("logged out");
   }
 }
 async function logout(req, res) {
   if (req.isAuthenticated()) {
-    req.logout();
-    res.status(200).send("logged out");
+    req.logout(function (err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/api/auth/status");
+    });
   } else {
     res.status(401).send({ success: false });
   }
